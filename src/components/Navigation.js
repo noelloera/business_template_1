19aae63f5a12dc9703b87c4a518a12ba3c283534
Noel Loera <50557498+noelloera@users.noreply.github.com>
2021-01-08 19:16:28 -0700

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,14 +3,7 @@ import { Navbar, Nav } from "react-bootstrap";
 const Navigation = (props) => {
   return (
     <>
-      <style type="text/css">
-        {`
-          .navbar-dark-tratra{
-            background-color: purple;
-        }
-        `}
-      </style>
-      <Navbar bg="dark" variant="tratra" className="d-flex">
+      <Navbar bg="dark" variant="dark" className="d-flex">
         <Navbar.Brand className="mr-auto p-2" href="#">
           Business Name
         </Navbar.Brand>
